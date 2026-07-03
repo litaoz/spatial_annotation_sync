f283f627025b84f314fd1eee392092a8ee6ff3d8
litaoz <litaored@gmail.com>
2026-07-03 15:20:58 -0400

    edit and merge

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod models;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, hash_map::Entry};
+use chrono::{DateTime, Utc};
 use uuid::Uuid;
+
 use models::SpatialAnnotationInternal;
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd,  Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd,  Hash)]
 pub struct UserId(pub Uuid);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Hash)]
@@ -28,6 +30,24 @@ impl SpatialAnnotation {
 
     const fn new_internal(id: Option<AnnotationId>, coord: Option<Point>, text: Option<String>) -> Self {
         Self {id, coord, text}
+    }
+
+    #[must_use]
+    pub const fn get_coord(&self) -> Option<&Point> {
+        self.coord.as_ref()
+    }
+
+    pub const fn update_coord(&mut self, coord: Option<Point>) {
+        self.coord = coord;
+    }
+
+    #[must_use]
+    pub const fn get_text(&self) -> Option<&String> {
+        self.text.as_ref()
+    }
+
+    pub fn update_text(&mut self, text: Option<String>) {
+        self.text = text;
     }
 }
 
@@ -65,7 +85,11 @@ impl SpatialEnvironment {
     }
 
     pub fn create_annotation(&mut self, annotation: SpatialAnnotation) -> AnnotationId {
-        let annotation = SpatialAnnotationInternal::new(annotation, self.user);
+        Self::create_annotation_with_datetime(self, annotation, Utc::now())
+    }
+
+    fn create_annotation_with_datetime(&mut self, annotation: SpatialAnnotation, datetime: DateTime<Utc>) -> AnnotationId {
+        let annotation = SpatialAnnotationInternal::new(annotation, self.user, datetime);
         let id = annotation.id;
         self.data.insert(id, annotation);
         id
@@ -75,6 +99,31 @@ impl SpatialEnvironment {
         self.data
             .get(&annotation_id)
             .map(From::from)
+    }
+
+    pub fn update_annotation(&mut self, annotation: SpatialAnnotation) -> Option<SpatialAnnotation> {
+        let a_clone = annotation.clone();
+        let Some(id) = annotation.id else {
+            self.create_annotation(annotation);
+            return Some(a_clone);
+
+        };
+        // let annotation = SpatialAnnotationInternal::new(annotation, self.user);
+        let _entry = self.data
+            .entry(id).and_modify(|a| a.update(annotation, self.user));
+        Some(a_clone)
+    }
+
+    pub fn merge(&mut self, other: Self) {
+        // for every annotiation in the other env, merge
+        for (id, incoming) in other.data {
+            match self.data.entry(id) {
+                Entry::Occupied(mut e) => e.get_mut().merge(incoming),
+                Entry::Vacant(e) => {
+                    e.insert(incoming);
+                }
+            }
+        }
     }
 }
 
@@ -87,6 +136,7 @@ impl Default for SpatialEnvironment {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use chrono::{TimeZone, Utc};
 
     #[test]
     fn test_create_annotation() {
@@ -126,9 +176,32 @@ mod tests {
 
     #[test]
     fn test_update_annotation() {
+        let mut env = SpatialEnvironment::new();
+        let a1 = SpatialAnnotation::new(
+            None,
+            Point(0, 0),
+            String::from("Home")
+        );
+        let a1_expected = SpatialAnnotation{
+            coord: Some(Point(1, 0)),
+            ..a1.clone()
+        };
+
+        let _ = env.create_annotation(a1);
+        let a1 = env.update_annotation(a1_expected.clone())
+            .expect("update_annotation should return OK");
+        assert_eq!(a1, a1_expected);
     }
 
     #[test]
     fn test_delete_annotation() {
     }
+
+    #[test]
+    fn test_merge() {
+        let _env_left = SpatialEnvironment::new();
+        let _env_right = SpatialEnvironment::new();
+
+    }
+
 }
--- src/models.rs
+++ src/models.rs
@@ -3,26 +3,49 @@ use chrono::{Utc, DateTime};
 
 use super::{Point, SpatialAnnotation, UserId, AnnotationId};
 
+#[derive(Debug, Clone, PartialEq)]
 struct LwwRegister<T> {
     value: T,
     last_modified_user: UserId,
     last_modified_time: DateTime<Utc>
 }
 
-impl<T: Clone> LwwRegister<T> {
-    fn new(value: T, user: UserId) -> Self {
+impl<T: Clone + PartialEq> LwwRegister<T> {
+    // fn new(value: T, user: UserId) -> Self {
+    //     Self::new_with_datetime(value, user, Utc::now())
+    // }
+
+    const fn new_with_datetime(value: T, user: UserId, datetime: DateTime<Utc>) -> Self {
         Self {
             value,
             last_modified_user: user,
-            last_modified_time: Utc::now()
+            last_modified_time: datetime
         }
     }
 
     fn clone_value(&self) -> T {
         self.value.clone()
     }
+
+    fn update(&mut self, value: T, user: UserId) {
+        if self.value == value { return; }
+        self.value = value;
+        self.last_modified_user = user;
+        self.last_modified_time = Utc::now();
+    }
+
+    fn merge(&mut self, other: Self) {
+        let other_is_newer = other.last_modified_time > self.last_modified_time;
+        let other_is_equal_and_lower_user_id = other.last_modified_time == self.last_modified_time && other.last_modified_user < self.last_modified_user;
+        if other_is_newer || other_is_equal_and_lower_user_id {
+            self.value = other.value;
+            self.last_modified_time = other.last_modified_time;
+            self.last_modified_user = other.last_modified_user;
+        }
+    }
 }
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct SpatialAnnotationInternal {
     pub id: AnnotationId,
     coord: LwwRegister<Option<Point>>,
@@ -30,14 +53,32 @@ pub struct SpatialAnnotationInternal {
 }
 
 impl SpatialAnnotationInternal {
-    pub fn new(value: SpatialAnnotation, user: UserId) -> Self {
-        let coord = LwwRegister::new(value.coord, user);
-        let text = LwwRegister::new(value.text, user);
-        Self {
-            id: AnnotationId(Uuid::new_v4()),
-            coord,
-            text,
-        }
+    // pub fn new(value: SpatialAnnotation, user: UserId) -> Self {
+    //     let coord = LwwRegister::new(value.coord, user);
+    //     let text = LwwRegister::new(value.text, user);
+    //     Self {
+    //         id: AnnotationId(Uuid::new_v4()),
+    //         coord,
+    //         text,
+    //     }
+    // }
+
+    pub(super) fn new(value: SpatialAnnotation, user: UserId, datetime: DateTime<Utc>) -> Self {
+        let id = value.id
+            .unwrap_or_else(|| AnnotationId(Uuid::new_v4()));
+        let coord = LwwRegister::new_with_datetime(value.coord, user, datetime);
+        let text = LwwRegister::new_with_datetime(value.text, user, datetime);
+        Self { id, coord, text }
+    }
+
+    pub fn update(&mut self, other: SpatialAnnotation, user: UserId) {
+        self.coord.update(other.coord, user);
+        self.text.update(other.text, user);
+    }
+
+    pub fn merge(&mut self, other: Self) {
+        self.coord.merge(other.coord);
+        self.text.merge(other.text);
     }
 }
 
@@ -48,5 +89,66 @@ impl From<&SpatialAnnotationInternal> for SpatialAnnotation {
             value.coord.clone_value(),
             value.text.clone_value()
         )
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use chrono::{TimeZone, Timelike};
+
+    use super::*;
+
+    #[test]
+    fn test_merge() {
+        let annotation_id = Some(AnnotationId(Uuid::from_u128(1)));
+        let left_values = (Point(0, 0), String::from("A"));
+        let right_values = (Point(5, 5), String::from("B"));
+
+        let baseline_user = UserId(Uuid::from_u128(1));
+        let earlier_user = UserId(Uuid::from_u128(0));
+        let later_user = UserId(Uuid::from_u128(2));
+        let baseline_date: DateTime<Utc> = Utc
+            .with_ymd_and_hms(2026, 1, 1, 0, 0, 1)
+            .single()
+            .expect("A valid UTC datetime");
+        let earlier_date = baseline_date.with_second(0).expect("A valid UTC datetime");
+        let later_date = baseline_date.with_second(2).expect("A valid UTC datetime");
+
+        let tests = [
+            ((baseline_user, baseline_date), (earlier_user,  earlier_date),  "left"),
+            ((baseline_user, baseline_date), (earlier_user,  baseline_date), "right"),
+            ((baseline_user, baseline_date), (earlier_user,  later_date),    "right"),
+            ((baseline_user, baseline_date), (baseline_user, earlier_date),  "left"),
+            ((baseline_user, baseline_date), (baseline_user, baseline_date), "left"),
+            ((baseline_user, baseline_date), (baseline_user, later_date),    "right"),
+            ((baseline_user, baseline_date), (later_user,    earlier_date),  "left"),
+            ((baseline_user, baseline_date), (later_user,    baseline_date), "left"),
+            ((baseline_user, baseline_date), (later_user,    later_date),    "right"),
+        ];
+        for scenario in tests {
+            let ((left_user, left_date), (right_user, right_date), expected) = scenario;
+            let (left_coord, left_str) = left_values.clone();
+            let (right_coord, right_str) = right_values.clone();
+            let mut left = SpatialAnnotationInternal::new(
+                SpatialAnnotation::new(annotation_id, left_coord, left_str),
+                left_user,
+                left_date
+            );
+            let right = SpatialAnnotationInternal::new(
+                SpatialAnnotation::new(annotation_id, right_coord, right_str),
+                right_user,
+                right_date
+            );
+            let expected = match expected {
+                "left" => left.clone(),
+                "right" => right.clone(),
+                _ => panic!("Bad expected value")
+            };
+
+            left.merge(right);
+            assert_eq!(left, expected);
+        }
     }
 }
